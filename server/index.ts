72895c88ee2713dfd3b0efb523f421748f36dd5b
Marllon Ramos <marllonsteam@gmail.com>
2025-05-24 10:29:15 -0700

    fix: corrige path do diretório dist para produção

--- server/index.ts
+++ server/index.ts
@@ -59,8 +59,9 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 3000;
-  server.listen(port, () => {
-    log(`serving on port ${port}`);
+  const port = parseInt(process.env.PORT || '3000', 10);
+  const host = process.env.HOST || '0.0.0.0';
+  server.listen(port, host, () => {
+    log(`serving on ${host}:${port}`);
   });
 })();
